161b3faab5b46ecda450b4196fb4a8f8b8600cc2
Michael Davis <rubiksmaster65@yahoo.com>
2016-12-06 17:50:52 -0500

    Multiple threading example
    Uses multithreading to create and print object data

---
+++ MultipleObjectThreads.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class MultipleObjectThreads implements Runnable{
+    private int objectNumber;
+    public static void main(String[] args){
+        int nObjects;
+        try{
+            nObjects = Integer.parseInt(args[0]);
+        } catch (ArrayIndexOutOfBoundsException a) {
+            nObjects = 0;
+        }
+        Scanner console = new Scanner(System.in);
+        while (nObjects < 1 || nObjects > 96){
+            System.out.print("How many objects? (Please enter a number between 1 and 30): ");
+            try{
+                nObjects = console.nextInt();
+            } catch (NumberFormatException e){
+                System.out.println("Please enter a number.");
+                nObjects = 0;
+            }
+        }
+        System.out.println("There are "+nObjects+" objects: ");
+        MultipleObjectThreads[] objectArray =
+            new MultipleObjectThreads[nObjects];
+        Thread[] threads = new Thread[nObjects];
+        for (int i = 0; i < nObjects; i++){
+            objectArray[i] = new MultipleObjectThreads(i+1);
+            threads[i] = new Thread(objectArray[i]);
+            threads[i].start();
+        }
+    }
+
+    public MultipleObjectThreads(int objectNumber){
+        this.objectNumber = objectNumber;
+    }
+
+    @Override
+    public void run(){
+        System.out.printf("Object %3d is running on thread ID: %3d%n",
+            this.getObjectNumber(), Thread.currentThread().getId());
+    }
+
+    public int getObjectNumber(){
+        return objectNumber;
+    }
+}
