3c363a2a69a919a2b2670e5df9ef1ecdf692be0e
Michael Davis <rubiksmaster65@yahoo.com>
2017-09-17 16:07:04 -0400

    Remake the point iteration with ruby
    This was originally a python script

---
+++ ruby/point_iteration.rb
@@ -0,0 +1,14 @@
+def iterate (start)
+    n = start
+    p = 0
+    while n != p do
+        p = n
+        n = (start / Math::log(n, 2)).round
+        puts n
+    end
+end
+
+ARGV.each do |no|
+    iterate no.to_i
+    puts
+end
