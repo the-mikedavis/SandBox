6b7fc841ebf1bbd96642b9c191e2c690632d2240
Michael Davis <rubiksmaster65@yahoo.com>
2017-11-28 17:06:20 -0500

    Add my implementation of comp

---
+++ clojure/chapter_five.clj
@@ -0,0 +1,9 @@
+(ns chapter-five)
+
+(defn my-comp
+  "My implementation of the comp function"
+  [& functions]
+  (fn [& arguments]
+    (reduce (fn [arg fun] (fun arg))
+            (apply (last functions) arguments)
+            (rest reverse functions))))
