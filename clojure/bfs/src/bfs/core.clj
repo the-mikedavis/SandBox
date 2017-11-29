9eb9e4f8cb078355fa675fb735b225db8bffb806
Michael Davis <rubiksmaster65@yahoo.com>
2017-11-29 11:02:18 -0500

    Add a vector making function

--- clojure/bfs/src/bfs/core.clj
+++ clojure/bfs/src/bfs/core.clj
@@ -1,6 +1,11 @@
 (ns bfs.core
   (:gen-class))
 
+(defn make-vector
+  "Make a vector for <m, c, b>"
+  [missionaries cannibals boat]
+  (hash-map :m missionaries :c cannibals :b boat))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
