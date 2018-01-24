20cb5dca22a3850b2d98e79edd0084988e38dd6b
Michael Davis <mcd164@miami.edu>
2018-01-24 18:18:24 -0500

    Get through auth

--- phoenix/rumbl/web/controllers/user_controller.ex
+++ phoenix/rumbl/web/controllers/user_controller.ex
@@ -2,14 +2,11 @@ defmodule Rumbl.UserController do
   use Rumbl.Web, :controller
   alias Rumbl.User
 
+  plug :authenticate_user when action in [:index, :show]
+
   def index(conn, _params) do
     users = Repo.all(User)
     render conn, "index.html", users: users
-  end
-
-  def new(conn, _params) do
-    changeset = User.changeset %User{}
-    render conn, "new.html", changeset: changeset
   end
 
   def show(conn, %{"id" => id}) when id != "new" do
@@ -17,4 +14,21 @@ defmodule Rumbl.UserController do
     render conn, "show.html", user: user
   end
 
+  def new(conn, _params) do
+    changeset = User.changeset %User{}
+    render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"user" => user_params}) do
+    changeset = User.registration_changeset(%User{}, user_params)
+    case Repo.insert(changeset) do
+      { :ok, user } ->
+        conn
+        |> Rumbl.Auth.login(user)
+        |> put_flash(:info, "#{user.name} created!")
+        |> redirect(to: user_path(conn, :index))
+      { :error, changeset } ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
 end
