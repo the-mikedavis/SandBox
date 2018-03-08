2af9d53d1b250c2d24c308d8bcc9e48dc6c192e6
Michael Davis <mcd164@miami.edu>
2018-03-08 13:38:14 -0500

    Change update condition to just "true"

--- java/InfLoop.java
+++ java/InfLoop.java
@@ -1,7 +1,7 @@
 public class InfLoop {
   public static void main (String[] args) {
     int x = -1;
-    for (int i = 0; i >= 0; i++) {
+    for (int i = 0; true; i++) {
       x = i + 1;
       System.out.println(i);
     }
